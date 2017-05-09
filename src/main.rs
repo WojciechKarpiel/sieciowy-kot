f76b48f2f5acf79c20ab2003467f8d81c54f5e79
Wojciech Karpiel <wkarpiel@student.agh.edu.pl>
2017-05-09 11:14:25 +0200

    opcje jeszcze lepsze

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ use std::fmt;
 const LISTEN_FLAG_SHORT: &str = "s";
 const LISTEN_FLAG_LONG: &str = "słuchajcie";
 const LISTEN_FLAG_DESC: &str = "przełączcie w tryb słuchania";
+const HELP_FLAG_SHORT: &str = "p";
+const HELP_FLAG_LONG: &str = "pomóżcie";
+const HELP_FLAG_DESC: &str = "wyświetla ten tekst";
 const PORT_OPTION_SHORT: &str = "p";
 const PORT_OPTION_LONG: &str = "port";
 const PORT_OPTION_DESC: &str =  "słuchajcie/wysyłajcie na port";
@@ -24,10 +27,7 @@ enum ProgramArgumentError {
 
 impl fmt::Display for ProgramArgumentError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            ProgramArgumentError::LackingPortError => fmt::Display::fmt("Brak portu", f),
-            ProgramArgumentError::LackingHostError => fmt::Display::fmt("Brak gościa", f)
-        }
+        fmt::Display::fmt(self.description(), f)
     }
 }
 
@@ -44,25 +44,41 @@ impl Error for ProgramArgumentError {
     }
 }
 
+#[derive(Debug)]
 enum Mode {
     Listen(Port),
     Send(Host, Port),
+    Help,
 }
 
+#[derive(Debug)]
 struct Host(String);
+#[derive(Debug)]
 struct Port(String);
 
 fn main() {
-    println!("Hello, world!");
-    let mode = parse_args();
+    let options: Options = prepare_options();
+    let arguments: Vec<String> = std::env::args().collect();
+    let program_name =  arguments[0].clone();
+    match parse_arguments(&options, arguments) {
+        Ok(Mode::Listen(port)) => listen(port),
+        Ok(Mode::Send(host, port)) => send(host, port),
+        Ok(Mode::Help) => print_help(&program_name, &options),
+        Err(error) => {
+            println!("{}", error);
+            print_help(&program_name, &options);
+        }
+    }
 }
 
-fn parse_args() -> Result<Mode, ProgramArgumentError> {
-    let args: Vec<String> = std::env::args().collect();
-    let mut options = Options::new();
+fn prepare_options() -> Options {
+    let mut options: Options = Options::new();
     options.optflag(LISTEN_FLAG_SHORT,
                     LISTEN_FLAG_LONG,
                     LISTEN_FLAG_DESC);
+    options.optflag(HELP_FLAG_SHORT,
+                    HELP_FLAG_LONG,
+                    HELP_FLAG_DESC);
     options.optopt(PORT_OPTION_SHORT,
                    PORT_OPTION_LONG,
                    PORT_OPTION_DESC,
@@ -71,13 +87,34 @@ fn parse_args() -> Result<Mode, ProgramArgumentError> {
                    HOST_OPTION_LONG,
                    HOST_OPTION_DESC,
                    HOST_OPTION_HINT);
-    let matches = match  options.parse(&args[1..]) {
+    options                
+}
+
+fn listen(Port(port): Port) {
+    println!("listen {}", port);
+}
+
+fn send(Host(host): Host, Port(port): Port) {
+    println!("{} {}",host,port);
+}
+
+fn print_help(program_name: &str, options: &Options) {
+    let description = format!("Sposób użycia: {} [OPCJE]", program_name);
+    println!("{}", options.usage(&description));
+}
+
+fn parse_arguments( options: &Options,arguments: Vec<String>) -> Result<Mode, ProgramArgumentError> {
+    let matches = match  options.parse(&arguments[1..]) {
         Ok(m) => m,
         Err(err) =>  panic!(err.to_string())
     };
     
+    if matches.opt_present(HELP_FLAG_SHORT) {
+        return Ok(Mode::Help)
+    }
+
     let port: Port = match  matches.opt_str(PORT_OPTION_SHORT) {
-        Some(port) =>  Port(port) ,
+        Some(port) =>  Port(port),
         None => return Err(ProgramArgumentError::LackingPortError)
     };
 
@@ -90,8 +127,4 @@ fn parse_args() -> Result<Mode, ProgramArgumentError> {
     } else {
         Ok(Mode::Listen(port))
     }
-}
-
-fn print_help_and_exit() {
-
 }
